45c408d587272985ae9e7033bfa5ddab2df223e5
bazenkov <n.bazenkov@yandex.ru>
2022-02-01 22:11:09 +0300

    Subscription branch is added. Commands and states are tested, but no changes in the database are done currently.
    *** New classes ***
    - bot.Subscribe - incapsulates the branch states and local data
    
    New functions:
    - bot.process_subscribe
    - process_subscribe_publishers
    - process_subscribe_period

--- bot.py
+++ bot.py
@@ -22,16 +22,35 @@ dp = Dispatcher(bot, storage=storage)
 # States
 class Dialog(StatesGroup):
     main = State('main')  # The main dialog
+    # /Whatsnew chain:
+    # main -> /Whatsnew -> news -> Неделя/Месяц -> publisher -> МИФ/Альпина ->
+    # -> book_preview -> По одной/Все -> show_mode
     news = State('news')  # Appears after \Whatsnews command
+    publisher = State('publisher')  # Chose publisher after chosing time period
 
-def send_to_channel(text:str):
+    # main -> /Unsubscribe -> unsubscribe_publishers -> МИФ/Альпина -> main
+
+
+class Subscribe(StatesGroup):
+    # /Subscribe chain:
+    # main -> /Subscribe -> subscribe_publishers -> МИФ/Альпина ->
+    # -> subscribe_perido -> Месяц/неделя -> main
+    publishers = State('subscribe_publishers')
+    period = State('subscribe_period')
+    current_publisher = None
+
+
+def send_to_channel(text: str):
     executor.start(dp, main(text))
 
-async def main(text:str):
+
+async def main(text: str):
     await send_message(config.chat_id, text)
+
 
 async def send_message(channel_id: int, text: str):
     await bot.send_message(channel_id, text)
+
 
 @dp.message_handler(commands=['Start'], state='*')
 async def process_start(message: types.Message):
@@ -88,8 +107,39 @@ async def process_subscribe(message: types.Message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     for p in publishers:
         markup.add(p['name'])
+    await Subscribe.publishers.set()
     await message.answer('На какое издательство подписаться?',
                          reply_markup=markup)
+
+
+@dp.message_handler(state=Subscribe.publishers)
+async def process_subscribe_publishers(message: types.Message):
+    publishers = [p['name'] for p in data.get_all_publishers(config.db_name)]
+    if message.text in publishers:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+        markup.add("Раз в месяц", "Раз в неделю")
+        Subscribe.current_publisher = message.text
+        await Subscribe.period.set()
+        await message.answer("Как часто присылать уведомления?", reply_markup=markup)
+    else:
+        await message.answer('Такого издательства не существует')
+
+
+@dp.message_handler(state=Subscribe.period)
+async def process_subscribe_period(message: types.Message):
+    if message.text.lower() in {"раз в месяц", "месяц"}:
+        period = "month"
+    elif message.text.lower() in {"раз в неделю", "неделя"}:
+        period = "week"
+    else:
+        await message.answer('Выбран неверный период, допустимые значения: \
+                             раз в месяц, раз в неделю')
+        return
+    # add subscription to database
+    await Dialog.main.set()
+    await message.answer(f"Вы подписаны на издательство {Subscribe.current_publisher}",
+                         reply_markup=types.ReplyKeyboardRemove())
+    Subscribe.current_publisher = None
 
 
 @dp.message_handler(commands=['logo'])
