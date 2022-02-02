479cbd315ac4569625c347fb7a954401a60dbe76
rs <salakhiev.ruslan@gmail.com>
2022-02-02 21:41:59 +0300

    New parser: Corpus

--- parser.py
+++ parser.py
@@ -3,13 +3,15 @@ from time import sleep
 import requests
 from bs4 import BeautifulSoup
 
-from data import add_book, find_publisher, find_book
+from data import add_book, find_publisher, find_book, add_publisher
 import config
 from bot import send_to_channel
 
 
 def parse_mif(db_name, publisher_name):
     url = 'https://www.mann-ivanov-ferber.ru/books/new/'
+
+    if find_publisher(db_name,publisher_name) is None: add_publisher(db_name,'МИФ')
 
     r = requests.get(url)
     catalog_xml = BeautifulSoup(r.text, 'lxml')
@@ -53,7 +55,53 @@ def parse_mif(db_name, publisher_name):
 
     return data, len(data), to_parse, len(books)
 
-def update_db(db_name, books,publisher_name):
+
+def parse_corpus(db_name, publisher_name):
+    url = 'https://www.corpus.ru/products/novinki/'
+
+    if find_publisher(db_name, publisher_name) is None: add_publisher(db_name, 'Corpus')
+
+    r = requests.get(url)
+    catalog_xml = BeautifulSoup(r.text, 'lxml')
+
+    books = catalog_xml.findAll('div', class_='book__item__container')
+
+    data = []
+    to_parse = 0
+    # парсинг каталога новинок
+    for book in books:
+        book_url = book.find('a').get('href')
+
+        if find_book(db_name, book_url) is None:
+            to_parse += 1
+            try:
+                title = book.find('div', class_='book__item__title').find('span').text.strip()
+                author = book.find('div', class_='book__item__author').find('span').text
+
+                r = requests.get(book_url)
+                bookpage_xml = BeautifulSoup(r.text, 'lxml')
+
+                image_url = bookpage_xml.find('div', class_='productzoom__top__info__image').find('a').get('href')
+
+                abstract_block = bookpage_xml.find('div', class_='rowBlockText').findAll()
+
+                short_abstract = abstract_block[0].text.replace('\xa0','')
+
+                full_abstract = ''
+                for text_part in abstract_block:
+                    full_abstract = full_abstract + text_part.text.replace('\xa0','') + '\n'
+
+
+                data.append([title, book_url, author, image_url, short_abstract, full_abstract, publisher_name])
+
+            except:
+                pass
+
+            sleep(2)
+    return data, len(data), to_parse, len(books)
+
+
+def update_db(db_name, books, publisher_name):
     update_count = 0
 
     if books == []:   # Проверяю отдает ли что-то парсер
@@ -90,6 +138,17 @@ if __name__ == '__main__':
     # Парсинг МИФа
     publisher_name = 'МИФ'
     books, parsed,to_parse, total  = parse_mif(config.db_name, publisher_name)
+    update_count = update_db(config.db_name, books, publisher_name)
+    status = check_status(parsed,to_parse, total,update_count)
+
+    text = str(f"{today}\n<b>{publisher_name}:</b> Парсер: {total}/{to_parse}/{parsed}. БД: {update_count} --- {status}")
+    # Отправка статистики в канал
+    send_to_channel(text)
+
+    # Парсинг Корпуса
+    publisher_name = 'Corpus'
+
+    books, parsed,to_parse, total  = parse_corpus(config.db_name, publisher_name)
     update_count = update_db(config.db_name, books, publisher_name)
     status = check_status(parsed,to_parse, total,update_count)
 
