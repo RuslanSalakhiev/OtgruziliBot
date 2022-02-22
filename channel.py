ed562f5e333a04c4cc60cb2026e0fdca9d546ca7
rs <salakhiev.ruslan@gmail.com>
2022-02-22 20:59:36 +0300

    schedule job

---
+++ channel.py
@@ -0,0 +1,24 @@
+import schedule
+from time import sleep
+
+from bot import send_to_channel
+from data import find_books,update_is_sent,find_publisher
+import config
+
+def send_to_channel_job():
+    for book in find_books(config.db_name, is_sent_to_channel='0'):
+        sent_message = f'<b>{book["title"]}</b>\n{book["author"]}\n<i>{book["pub_name"]}</i>'
+
+        send_to_channel(text=sent_message, photo_url=book['image_path'], message_type='photo', url=book['url'])
+
+        update_is_sent(config.db_name, book['url'])
+
+        sleep(4)
+
+
+schedule.every().day.at("20:58").do(send_to_channel_job)
+
+while True:
+    schedule.run_pending()
+    sleep(1)
+
