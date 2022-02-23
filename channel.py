8bf54b5d7069e9f5e27186996a47e7958129a3bc
rs <salakhiev.ruslan@gmail.com>
2022-02-23 09:31:47 +0300

    small fix

--- channel.py
+++ channel.py
@@ -11,7 +11,7 @@ def send_to_channel_job():
         sent_message = f'<b>{book["title"]}</b>\n{book["author"]}\n<i>{book["pub_name"]}</i>'
 
         send_to_channel(text=sent_message, photo_url=book['image_path'], message_type='photo', url=book['url'],
-                        chat_id=config.tech_chat_id, keyboard=True)
+                        chat_id=config.prod_chat_id, keyboard=True)
 
         update_is_sent(config.db_name, book['url'])
 
